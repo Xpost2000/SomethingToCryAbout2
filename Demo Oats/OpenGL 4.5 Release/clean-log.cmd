a0af4e6db3698aac6a372611f75cbacc4f2fab98
Xpost2000 <xpost2000cod@gmail.com>
2017-06-25 22:13:51 -0400

    Cleaning Slate

---
+++ Demo Oats/OpenGL 4.5 Release/clean-log.cmd
@@ -0,0 +1,6 @@
+@echo off
+cd util
+clear-log ../Oats_stderr.log
+echo Log File Cleared
+echo CMD Utility for Oats Engine
+pause
--- SomethingToCryAbout2/Game.cpp
+++ SomethingToCryAbout2/Game.cpp
@@ -7,19 +7,7 @@
 #include <ctime>
 int mx;
 int my;
-int score[2];
-float animatedTextX = 0.0;
-Ball ball(glm::vec2(1024/2 - 20, 768 / 2 - 20), glm::vec2(20), 300 , 47);
-Paddle a(glm::vec2( 40, 400), glm::vec2(10, 100));
-Paddle b(glm::vec2(1024 - 40, 400), glm::vec2(10, 100));
-struct DemoCoords{
-	float y1 = 400; // P1 Demo
-	float y2 = 400; // P2 Demo
-}set;
 
-std::string specialFx = "None";
-glm::vec3 textColor = { 0, 0, 0 };
-Button play(glm::vec2(382, 680), glm::vec2(340, 60), glm::vec3(255), glm::vec3(0), "Play Game");
 glQueryInfo info;
 Game::Game()
 {
@@ -53,7 +41,6 @@ Game::~Game(){
 }
 // Very Huge setup
 void Game::InitGame(){
-	play.SetID(1);
 	camera = new Camera2D(view, width, height);
 	fragment = new Shader(GL_FRAGMENT_SHADER);
 	vertex = new Shader(GL_VERTEX_SHADER);
@@ -126,37 +113,8 @@ void Game::UpdateGame(){
 	view = glm::mat4();
 	model = glm::mat4();
 	ClockTimer::Tick();
-	set.y1 += cos(ClockTimer::returnElaspedTime(TimeMeasure::TIME_SECONDS) / 3);
-	set.y2 += cos(ClockTimer::returnElaspedTime(TimeMeasure::TIME_SECONDS) * 0.2);
 	glitch = true;
 	if (inState == GameState::GAME_RUNNING){
-		glitch = false;
-		if (animatedTextX > 1024)
-		ball.Update(ClockTimer::returnDeltatime(TimeMeasure::TIME_SECONDS));
-		else{
-		//	animatedTextX += cos(ClockTimer::returnElaspedTime(TimeMeasure::TIME_SECONDS) / 10)*6;
-			animatedTextX += 220 * ClockTimer::returnDeltatime(TimeMeasure::TIME_SECONDS);
-		}
-		if (a.Collide(ball)){
-			ball.InvertVX();
-		}
-		if (b.Collide(ball)){
-			ball.InvertVX();
-		}
-		if (ball.pos.x + ball.size.x > 1024){
-			score[0] ++;
-			animatedTextX = 0;
-			a = Paddle(glm::vec2(40, 400), glm::vec2(10, 100));
-			b = Paddle(glm::vec2(1024 - 40, 400), glm::vec2(10, 100));
-			ball = Ball(glm::vec2(1024 / 2 - 20, 768 / 2 - 20), glm::vec2(20), 300, 47);
-		}
-		if (ball.pos.x < 0){
-			score[1] ++;
-			animatedTextX = 0;
-			a = Paddle(glm::vec2(40, 400), glm::vec2(10, 100));
-			b = Paddle(glm::vec2(1024 - 40, 400), glm::vec2(10, 100));
-			ball = Ball(glm::vec2(1024 / 2 - 20, 768 / 2 - 20), glm::vec2(20), 300, 47);
-		}
 	}
 }
 
@@ -174,19 +132,8 @@ void Game::DrawGame(){
 	view = camera->RetrieveMatrix(); // Call this to transfer matrix.
 	program->SetUniformMatrix4fv("view", glm::value_ptr(view));
 	if (inState == GameState::GAME_MENU){
-		renderer->Draw(glm::vec2(40, set.y1), glm::vec2(10, 100), 0, glm::vec3(255));
-		renderer->Draw(glm::vec2(1024-40, set.y2), glm::vec2(10, 100), 0, glm::vec3(255));
-		renderer->Draw(glm::vec2(set.y1, set.y2), glm::vec2(30), 0, glm::vec3(255));
 	}
 	if (inState == GameState::GAME_RUNNING){
-
-		renderer->Draw(a.GetPos(), a.GetSize(), 0);
-		renderer->Draw(b.GetPos(), b.GetSize(), 0);
-		renderer->Draw(ball.GetPos(), ball.GetSize(), 0);
-		arial->Render("Player 1 : " + std::to_string(score[0]), glm::vec2(30, 700), 1, glm::vec3( 0, 1, 0));
-		arial->Render("Player 2 : " + std::to_string(score[1]), glm::vec2(700, 700), 1, glm::vec3(0, 1, 0));
-		renderer->Draw(glm::vec2(animatedTextX, 768 / 2), glm::vec2(1024, 80), 0, glm::vec3(0));
-		cmcSans->Render("Game Time!", glm::vec2(animatedTextX, 768 / 2), 1, glm::vec3(1, 0, 0));
 	}
 	FrameBuffer->End();
 
@@ -203,14 +150,9 @@ void Game::DrawGame(){
 	smArial->Render("OpenGL Version: " + std::string(reinterpret_cast<const char*>(info.version.legacy.gl_api_version)), glm::vec2(250, 20), 1, glm::vec3(255));
 	smArial->Render("OpenGL-SL Version : " + std::string(reinterpret_cast<const char*>(info.glsl_lang_version)), glm::vec2(10, 40), 1, glm::vec3(255));
 	smArial->Render("OpenGL Renderer : " + std::string(reinterpret_cast<const char*>(info.renderer)), glm::vec2(10, 90), 1, glm::vec3(255));
-	
+
 	// Drawing a button like this
 	if (inState == GameState::GAME_PAUSE || inState == GameState::GAME_MENU){
-		cmcSans->Render("Oats PONG!", glm::vec2(340, 100), 1., glm::vec3(1, 0, 0));
-		arial->Render("Oats FW Test", glm::vec2(340, 180), 0.66, glm::vec3(0, 1, 0));
-		renderer->Draw(play.GetLoc(), play.GetSize(), 0, play.GetColor());
-		arial->Render(play.GetString(), play.GetTextLoc(), 1, play.GetTextColor());
-		animatedTextX = 0;
 	}
 	if (inState == GameState::GAME_RUNNING){
 		
@@ -235,29 +177,9 @@ void Game::HandleInput(){
 		case SDL_KEYUP:
 			break;
 		case SDL_MOUSEBUTTONDOWN:
-			if (inState == GameState::GAME_MENU){
-				if (play.IsInBounds(mx, my)){
-					SDL_Delay(500);
-					inState = GameState::GAME_RUNNING;
-				}
-			}
 			break;
 		default:
 			break;
 		}
 	});
-	if (animatedTextX > 1024){
-		input->isKeyPressed(SDL_SCANCODE_W, [&](){
-			a.MoveUp(ClockTimer::returnDeltatime(TimeMeasure::TIME_SECONDS));
-		});
-		input->isKeyPressed(SDL_SCANCODE_S, [&](){
-			a.MoveDown(ClockTimer::returnDeltatime(TimeMeasure::TIME_SECONDS));
-		});
-		input->isKeyPressed(SDL_SCANCODE_UP, [&](){
-			b.MoveUp(ClockTimer::returnDeltatime(TimeMeasure::TIME_SECONDS));
-		});
-		input->isKeyPressed(SDL_SCANCODE_DOWN, [&](){
-			b.MoveDown(ClockTimer::returnDeltatime(TimeMeasure::TIME_SECONDS));
-		});
-	}
 }
